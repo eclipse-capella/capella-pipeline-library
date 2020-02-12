bd5aeba5de8be35ab589ed4334e66c5bbe1359cd
Sandu Postaru <sandu.postaru@thalesgroup.com>
2020-02-12 16:24:44 +0100

    Update github.groovy
    - Add isPullRequest method
    
    Change-Id: Ib36c4fa8018e6caba9296334c44b5d15be84d427
    Signed-off-by: Sandu Postaru <sandu.postaru@thalesgroup.com>

--- vars/github.groovy
+++ vars/github.groovy
@@ -1,4 +1,13 @@
-def pullRequestComment(pullRequestId, message) {
+def isPullRequest() {
+  return "${BRANCH_NAME}".contains('PR-');
+}
+
+def getPullRequestId() {
+  return "${BRANCH_NAME}".replace('PR-', '')
+}
+
+def pullRequestComment(message) {
+  def pullRequestId = getPullRequestId()
   def apiURL = "https://api.github.com/repos/eclipse/capella/issues/${pullRequestId}/comments"
   
   withCredentials([string(credentialsId: '0dea5761-867c-44db-92fa-9304c81a8653', variable: 'password')]) {
@@ -10,33 +19,33 @@ def buildStartedComment() {
   def buildId = "${BUILD_KEY}-${BRANCH_NAME}-${BUILD_ID}".replaceAll('/','-')
   def message = ":rocket: Build [${buildId}](${BUILD_URL}) started!"
   
-  pullRequestComment(CHANGE_ID, message)
+  pullRequestComment(message)
 }
 
 def buildSuccessfullComment() {
   def buildDir = "${BUILD_KEY}-${BRANCH_NAME}-${BUILD_ID}".replaceAll('/','-')
   def message = ":thumbsup: Build [${buildDir}](${BUILD_URL}) is successfull! The product is available [here](http://download.eclipse.org/capella/core/products/nightly/${buildDir})."
   
-  pullRequestComment(CHANGE_ID, message)
+  pullRequestComment(message)
 }
 
 def buildFailedComment() {
   def buildId = "${BUILD_KEY}-${BRANCH_NAME}-${BUILD_ID}".replaceAll('/','-')
   def message = ":disappointed: Build [${buildId}](${BUILD_URL}) failed!"
   
-  pullRequestComment(CHANGE_ID, message)
+  pullRequestComment(message)
 }
 
 def buildUnstableComment() {
   def buildDir = "${BUILD_KEY}-${BRANCH_NAME}-${BUILD_ID}".replaceAll('/','-')
   def message = ":worried: Build [${buildDir}](${BUILD_URL}) is unstable! The product is available [here](http://download.eclipse.org/capella/core/products/nightly/${buildDir})."
   
-  pullRequestComment(CHANGE_ID, message)
+  pullRequestComment(message)
 }
 
 def buildAbortedComment() {
   def buildId = "${BUILD_KEY}-${BRANCH_NAME}-${BUILD_ID}".replaceAll('/','-')
   def message = ":neutral_face: Build [${buildId}](${BUILD_URL}) has been aborted!"
   
-  pullRequestComment(CHANGE_ID, message)
+  pullRequestComment(message)
 }
