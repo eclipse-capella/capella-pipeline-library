45a3a37851b3aff9c936afdbd8b58e5d2c631a97
Tu Ton <minhtutonthat@gmail.com>
2021-07-15 16:01:47 +0200

    Adapt pipeline library to take into account tar format
    Change-Id: Ie999ecda0f5d3215800903093485a7e561d4d17a
    Signed-off-by: Tu Ton <minhtutonthat@gmail.com>
    
    
    Change-Id: Ida1f39b1bb828ad10b4bce19ac4318a2b001a923
    Signed-off-by: T0189609 <T0189609@W602510.NTSIEGE.HQ.THALES>

--- vars/capella.groovy
+++ vars/capella.groovy
@@ -13,7 +13,11 @@ def getDownloadURL(branch = "master", platform = "win", proxy = ""){
           break
       
       case ~/linux/:
-          regex = /(capella-[\d.]*-linux-gtk-x86_64.tar.gz)/
+          if (branch.startsWith('1.')) {
+              regex = /(capella-[\d.]*-linux-gtk-x86_64.zip)/
+          } else {
+            regex = /(capella-[\d.]*-linux-gtk-x86_64.tar.gz)/
+          }
           break
           
       case ~/win/:
