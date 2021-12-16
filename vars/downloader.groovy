8cd82926d9e185cc78a88fbeb42703a13c375525
Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>
2021-12-16 15:55:46 +0100

    #31 Setup download library to provide JDK 17
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Arnaud Dieumegard <arnaud.dieumegard@obeo.fr>

--- vars/downloader.groovy
+++ vars/downloader.groovy
@@ -41,3 +41,65 @@ def downloadMacJDK(jdkMacFolder) {
   println "Mac JDK downloaded to ${jdkMacFolder}/jre"
 	sh "ls ${jdkMacFolder}/jre"
 }
+
+/**
+ * Download Temurin JDK 17 version in <code>jdkFolder</code> folder for operating system <code>os</code>
+ * @param jdkFolder
+ * @param os The operating system. Expected one of 'win', 'linux', 'mac'
+ * @return Nothing but extracts the downloaded JDK to <code>${jdkFolder}/jre</code>.
+ */
+def downloadTemurinJDK17(jdkFolder, os) {
+    def jdk = 'jdk-17.0.1+12-jre'
+    	
+    def jdkWinURL   = 'https://api.adoptium.net/v3/binary/latest/17/ga/windows/x64/jre/hotspot/normal/eclipse'
+    def jdkLinuxURL = 'https://api.adoptium.net/v3/binary/latest/17/ga/linux/x64/jre/hotspot/normal/eclipse'
+    def jdkMacURL   = 'https://api.adoptium.net/v3/binary/latest/17/ga/mac/x64/jre/hotspot/normal/eclipse'
+    
+    def jdkArchive = 'jdk'
+    def jdkURL = ''
+    
+    switch (os) {
+        case 'win':
+            jdkURL = jdkWinURL
+            jdkArchive += os + '.zip'
+            break;
+        case 'mac':
+            jdkURL = jdkMacURL
+            jdkArchive += os + '.tar.gz'
+            break;
+        case 'linux':
+            jdkURL = jdkLinuxURL
+            jdkArchive += os + '.tar.gz'
+            break;
+        default:
+            return;
+    }
+    
+    sh "curl -L -k -o ${jdkArchive} ${jdkURL}"
+    sh "ls -la ${jdkArchive}"
+    
+    def osPrint = ''
+    switch (os) {
+        case 'win':
+            sh "unzip -q ${jdkArchive} -d ${jdkFolder}"
+            osPrint = 'Win'
+            break;
+        case 'mac':
+            sh "mkdir ${jdkFolder}"
+            sh "tar xzf ${jdkArchive} -C ${jdkFolder}"
+            osPrint = 'Mac'
+            break;
+        case 'linux':
+            sh "mkdir ${jdkFolder}"
+            sh "tar xzf ${jdkArchive} -C ${jdkFolder}"
+            osPrint = 'Linux'
+            break;
+        default:
+            break;
+    }
+    
+    sh "mv ${jdkFolder}/${jdk} ${jdkFolder}/jre"
+    println "${osPrint} JDK downloaded to ${jdkFolder}/jre"
+    sh "ls ${jdkFolder}/jre"
+    
+}
