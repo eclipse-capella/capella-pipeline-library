f9953c557a9e22f93c0f381ff560756de2421a56
Philippe DUL <philippe.dul@thalesgroup.com>
2023-05-18 14:08:10 +0200

    Fix tester

--- vars/tester.groovy
+++ vars/tester.groovy
@@ -49,8 +49,8 @@ private def getJunitCmdTemplate(String capellaProductPath, String applicationPar
   
   return "sleep 10 && " +
     "java " +
-      "${jacocoParameters}" +
-      "-Xms1024m -Xmx3500m" +
+      "${jacocoParameters} " +
+      "-Xms1024m -Xmx3500m " +
       "-ea -da:org.eclipse.emf.common.util.URI " + // bug GMF 438712
       "-Declipse.p2.data.area=@config.dir/../p2 " +
       "-Dfile.encoding=Cp1252 " +
