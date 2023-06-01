2d86ea07a6777a035a52a2feb6e8088955b45a8f
Philippe DUL <philippe.dul@thalesgroup.com>
2023-06-01 14:46:45 +0200

    [releng] Update memory

--- vars/tester.groovy
+++ vars/tester.groovy
@@ -50,7 +50,7 @@ private def getJunitCmdTemplate(String capellaProductPath, String applicationPar
   return "sleep 10 && " +
     "java " +
       "${jacocoParameters} " +
-      "-Xms1024m -Xmx3500m " +
+      "-Xms1024m -Xmx3000m " +
       "-ea -da:org.eclipse.emf.common.util.URI " + // bug GMF 438712
       "-Declipse.p2.data.area=@config.dir/../p2 " +
       "-Dfile.encoding=Cp1252 " +
